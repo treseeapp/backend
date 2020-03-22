36cc5ea4606dc7906a82c38288034b8013be0f12
joanpacheco <joanantonipacheco@gmail.com>
2020-03-22 12:43:20 +0100

    Añadido el TokenFilter a la configuración, con las rutas en las que actuará el Filter.

--- src/main/java/com/tresee/backend/config/AppConfig.java
+++ src/main/java/com/tresee/backend/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.tresee.backend.config;
 
 
+import com.tresee.backend.filter.TokenFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,18 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-//    @Bean
-//    public TokenFilter getFilter() {
-//        return new TokenFilter();
-//    }
+    @Bean
+    public TokenFilter getFilter() {
+        return new TokenFilter();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        /*
-         * Aqui van todos los interceptors que creemos.
-         * */
 
-//        registry.addInterceptor(getFilter()).addPathPatterns("/p/**");
+        registry.addInterceptor(getFilter()).addPathPatterns("/p/**");
 
     }
 }
