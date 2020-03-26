dbdf0194db082cbab13c0da169f6e1363d73b36e
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-03-26 10:04:22 +0100

    Añadimos en el EmpresaManager.java el crear un objeto empresa a partir de un JSON, tambien lo que hacemos es Crear el Create en el manager
    En el empresa controller añadimos el endpoint para crear empresas. Este endpoint nos permitira recibir un json que se convertira en una empresa dentro de nuestr DDBB

--- src/main/java/com/tresee/backend/controller/EmpresaController.java
+++ src/main/java/com/tresee/backend/controller/EmpresaController.java
@@ -1,7 +1,11 @@
 package com.tresee.backend.controller;
 
+import com.tresee.backend.enitty.Empresa;
+import com.tresee.backend.manager.EmpresaManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmpresaController {
 
+    @Autowired
+    private EmpresaManager empresaManager;
 
     @PostMapping("/admin/empresas")
+    @Transactional
     public ResponseEntity<String> addEmpresa(@RequestBody String json) {
+        Empresa empresa = empresaManager.fromJsonCreate(json);
 
+        /*
+         * Comprobamos que recibimos los parametros obligatorios para crear una empresa
+         * */
+        if (empresa.getNombre() == null
+                || empresa.getContacto() == null)
+            return new ResponseEntity<>("No se han recibido los parametros obligatorios", HttpStatus.BAD_REQUEST);
 
+        empresaManager.create(empresa);
         return new ResponseEntity<>("Empresa creada correctamente", HttpStatus.OK);
     }
 }
--- src/main/java/com/tresee/backend/manager/EmpresaManager.java
+++ src/main/java/com/tresee/backend/manager/EmpresaManager.java
@@ -1,13 +1,42 @@
 package com.tresee.backend.manager;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.tresee.backend.enitty.Empresa;
+import com.tresee.backend.repository.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class EmpresaManager {
     @Autowired
     private Gson gson;
 
+    @Autowired
+    private EmpresaRepository empresaRepository;
 
+    public void create(Empresa empresa) {
+        empresaRepository.save(empresa);
+    }
+
+    public Empresa fromJsonCreate(String json) {
+        Empresa empresa = new Empresa();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+        if (jsonObject.get("nombre") != null) {
+            empresa.setNombre(jsonObject.get("nombre").getAsString());
+        }
+        if (jsonObject.get("contacto") != null) {
+            empresa.setContacto(jsonObject.get("contacto").getAsString());
+        }
+        if (jsonObject.get("fechaInicioPracticas") != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/d");
+            LocalDate date = LocalDate.parse(jsonObject.get("fechaInicioPracticas").getAsString(), formatter);
+            empresa.setInicioPracticas(date);
+        }
+
+        return empresa;
+    }
 }
