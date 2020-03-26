cfd1fbea2542d6f2d9d24147f0ec39495b935185
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-03-26 13:31:29 +0100

    Añadimos el endpoint /private/usuario - PUT el cual permitira modificar la informacion del perfil de uno mismo Faltan hacer comprobaciones como Si recibimos los campos obligatorios

--- src/main/java/com/tresee/backend/controller/UsuarioController.java
+++ src/main/java/com/tresee/backend/controller/UsuarioController.java
@@ -4,8 +4,12 @@ import com.tresee.backend.enitty.Usuario;
 import com.tresee.backend.manager.TokenManager;
 import com.tresee.backend.manager.UsuarioManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +31,31 @@ public class UsuarioController {
 
         return tokenManager.getUsuarioFromToken(token);
     }
+
+    @PutMapping("/private/usuario")
+    @Transactional
+    public ResponseEntity<String> updateMyInfo(@RequestBody String json, HttpServletRequest request) {
+
+        /*
+         * Cogemos el usuario del token, asi nos aseguramos de
+         * que el usuario que se modifica es a si mismo
+         * */
+        String token = request.getHeader("Authorization");
+        token = token.replace("Bearer ", "");
+        Usuario tokenUser = tokenManager.getUsuarioFromToken(token);
+
+        Usuario recivedInfo = usuarioManager.fromJson(json);
+
+        tokenUser.setNombre(recivedInfo.getNombre());
+        tokenUser.setApellidos(recivedInfo.getApellidos());
+        tokenUser.setDireccion(recivedInfo.getDireccion());
+        tokenUser.setDataNacimiento(recivedInfo.getDataNacimiento());
+        tokenUser.setGenero(recivedInfo.getGenero());
+
+        usuarioManager.update(tokenUser);
+        return new ResponseEntity<>("Información modificada correctamente", HttpStatus.OK);
+    }
+
 
     @GetMapping("/private/usuario/foto")
     @Transactional
--- src/main/java/com/tresee/backend/manager/UsuarioManager.java
+++ src/main/java/com/tresee/backend/manager/UsuarioManager.java
@@ -1,15 +1,22 @@
 package com.tresee.backend.manager;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.tresee.backend.enitty.Usuario;
+import com.tresee.backend.enitty.enums.Genero;
 import com.tresee.backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class UsuarioManager {
+    @Autowired
+    private Gson gson;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -36,5 +43,32 @@ public class UsuarioManager {
 
     public void delete(Usuario user) {
         usuarioRepository.delete(user);
+    }
+
+    public Usuario fromJson(String json) {
+        Usuario user = new Usuario();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+        if (jsonObject.get("nombre") != null) {
+            user.setNombre(jsonObject.get("nombre").getAsString());
+        }
+        if (jsonObject.get("apellidos") != null) {
+            user.setApellidos(jsonObject.get("apellidos").getAsString());
+        }
+        if (jsonObject.get("direccion") != null) {
+            user.setDireccion(jsonObject.get("direccion").getAsString());
+        }
+        if (jsonObject.get("dataNacimiento") != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/d");
+            LocalDate date = LocalDate.parse(jsonObject.get("dataNacimiento").getAsString(), formatter);
+            user.setDataNacimiento(date);
+        }
+        if (jsonObject.get("genero") != null) {
+            String genero = jsonObject.get("genero").getAsString();
+            if (genero.toUpperCase().equals(Genero.HOMBRE.toString())) user.setGenero(Genero.HOMBRE);
+            if (genero.toUpperCase().equals(Genero.MUJER.toString())) user.setGenero(Genero.MUJER);
+            if (genero.toUpperCase().equals(Genero.INDEFINIDO.toString())) user.setGenero(Genero.INDEFINIDO);
+        }
+
+        return user;
     }
 }
